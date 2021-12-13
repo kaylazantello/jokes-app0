f01688c0963522c06fa3d2a675ea20ce5b0002fd
kaylazantello <59740873+kaylazantello@users.noreply.github.com>
2021-12-13 16:13:07 -0700

    Updated credentials for Heroku JawsDB mySQL

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,11 @@
 <?php
 
  // four variables to connect to the database
- $host = "localhost";
+ $host = "uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
  //$port = 3306;
- $username = "root";
- $password = "root";
- $database = "test";
+ $username = "gg7gx2hjbb1bllnm";
+ $password = "sz4vxinztf292blj";
+ $database = "u4gnhd2evsjo6soi";
 
  // create a database instance
  $mysqli = new mysqli($host, $username, $password, $database);
